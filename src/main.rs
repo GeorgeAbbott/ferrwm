56b4bb3825dd21e5e75ab4578fedcc45d196f897
self <sateoki@protonmail.com>
2021-11-27 14:43:41 +0000

    Half implemented main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// Modules
+mod structs;
+mod tagmasks;
+mod enums;
+mod consts;
+
+// Usings
+use std::env;
+use die::die;
+
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<String> = env::args().collect();
+    let argc = argv.len();
+    
+    if argc == 2 && argv[1] != "-v" {
+        die!("riodwm-{}", consts::VERSION);
+    }
+    else if argc != 1 {
+        die!("usage: dwm [-v]");
+    }
+
+    // todo: rest of main
+
 }
