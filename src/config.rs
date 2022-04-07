f4990d73855a5a97a4725e54e7f005bf0520623b
otg <otg@sateoki.xyz>
2022-04-07 18:06:07 +0100

    Some slight corrections to config

--- src/config.rs
+++ src/config.rs
@@ -7,14 +7,25 @@ pub const BORDERPX: i32             = 1;    /* border pixel of windows */
 pub const GAPPX: i32                = 24;   /* gaps between windows */
 pub const SNAP: i32                 = 32;   /* snap pixel */
 pub const SHOW_BAR: bool            = true;
-pub const BAR_POSITION: BarPosition = Top;
+pub const BAR_POSITION: BarPosition = BarPosition::Top;
 
 pub const FONTS: &[&str]            = &["Liberation Mono:size=11"];
 pub const DMENU_FONT: &str          = "Liberation Mono;size=11";
 
 //pub const COLORS: &[&[&str]] =
 
-pub const TAGS: &[] = &[ "1", "2", "3", "4", "5", "6", "7", "8", "9", ];
+pub const TAGS: &[&str] = 
+&[ 
+    "1", 
+    "2", 
+    "3", 
+    "4", 
+    "5", 
+    "6", 
+    "7", 
+    "8", 
+    "9",
+];
                             
 
 
