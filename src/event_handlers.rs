4e1299c826cdf1a40ed94eecff0a8bf6faaf64a3
self <self@sateoki.xyz>
2022-05-28 21:26:29 +0100

    Formatting

--- src/event_handlers.rs
+++ src/event_handlers.rs
@@ -1,7 +1,15 @@
-// Holds functions to handle events which are called from handle_event 
+// Holds functions to handle events which are called from handle_event
 // function in main.rs.
 
-use x11rb::protocol::{Event, xproto::{ButtonPressEvent, ClientMessageEvent, ConfigureRequestEvent, ConfigureNotifyEvent, DestroyNotifyEvent, EnterNotifyEvent, ExposeEvent, FocusInEvent, KeyPressEvent, MappingNotifyEvent, MapRequestEvent, MotionNotifyEvent, PropertyNotifyEvent, UnmapNotifyEvent}};
+use x11rb::protocol::{
+    xproto::{
+        ButtonPressEvent, ClientMessageEvent, ConfigureNotifyEvent, ConfigureRequestEvent,
+        DestroyNotifyEvent, EnterNotifyEvent, ExposeEvent, FocusInEvent, KeyPressEvent,
+        MapRequestEvent, MappingNotifyEvent, MotionNotifyEvent, PropertyNotifyEvent,
+        UnmapNotifyEvent,
+    },
+    Event,
+};
 
 fn button_press(event: ButtonPressEvent) {}
 fn client_message(event: ClientMessageEvent) {}
@@ -17,4 +25,3 @@ fn map_request(event: MapRequestEvent) {}
 fn motion_notify(event: MotionNotifyEvent) {}
 fn property_notify(event: PropertyNotifyEvent) {}
 fn unmap_notify(event: UnmapNotifyEvent) {}
-
