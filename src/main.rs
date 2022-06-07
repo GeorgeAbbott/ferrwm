8c348e8f54d170ba3f237a1fd1b7d77021d1fc98
self <self@sateoki.xyz>
2022-06-07 22:05:11 +0100

    Basically commenting out a lot of things

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
 mod event_handlers;
 mod enums;
 mod consts;
-mod drawable;
-mod config;
-mod types;
-mod wrapx;
+// mod drawable;
+// mod config;
+// mod types;
+// mod wrapx;
 
 // Usings
 use std::env;
@@ -18,19 +18,19 @@ use openbsd::pledge;
 // use x11::xlib::*;
 // use x11::keysym::*;
 // use x11::*;
-use x11rb;
+use x11rb::{self, COPY_DEPTH_FROM_PARENT};
 use x11rb::connection::Connection;
 use x11rb::protocol::Event;
 use x11rb::protocol::randr::{MonitorInfo, get_screen_info};
-use x11rb::protocol::xproto::{Atom, Screen};
+use x11rb::protocol::xproto::{Atom, Screen, ConnectionExt, WindowClass, CreateWindowAux};
 use x11rb::rust_connection::RustConnection;
 
 
 /* Wrapper Types */
-use crate::types::display::Display;
-use crate::types::fnt::Fnt;
-use crate::types::drw::Drw;
-use crate::config;
+// use crate::types::display::Display;
+// use crate::types::fnt::Fnt;
+// use crate::types::drw::Drw;
+// use crate::config;
 // use crate::xwrap::supports_locale;
 
 // Alias
@@ -60,27 +60,27 @@ fn setup(conn: &RustConnection) {
     /* clean up any zombies immediately */
     // sigchld(0); // TODO: ???
     /* init screen */
-    let screen = DefaultScreen(display);
-    let screen_width = DisplayWidth(display, screen);
-    let screen_height = DisplayHeight(display, screen);
-    let root = RootWindow(display, screen);
-    let drawable = drawable::MyDrawable::new(display, screen, root, screen_width, screen_height); // TODO: replace with Drw::new
+    // let screen = DefaultScreen(display);
+    // let screen_width = DisplayWidth(display, screen);
+    // let screen_height = DisplayHeight(display, screen);
+    // let root = RootWindow(display, screen);
+    // let drawable = drawable::MyDrawable::new(display, screen, root, screen_width, screen_height); // TODO: replace with Drw::new
 
-    let font = drawable::Font::new(&drawable, 
-                                   config::FONTS); // TODO: replace w/ Fnt::new
-    if font.is_none() {
-        die!("no fonts could be loaded.");
-    } 
-    let font = font.unwrap();
+    // let font = drawable::Font::new(&drawable, 
+    //                                config::FONTS); // TODO: replace w/ Fnt::new
+    // if font.is_none() {
+    //     die!("no fonts could be loaded.");
+    // } 
+    // let font = font.unwrap();
 
 
-    let lrpad = drawable.fonts.height; // TODO: make these public
+    // let lrpad = drawable.fonts.height; // TODO: make these public
 
-    let bar_height = drawable.fonts.height + 2; // will not make global but instead pass to every func that needs it
-    updategeom(); // TODO
-    
-    /* init atoms */
-    todo!();
+    // let bar_height = drawable.fonts.height + 2; // will not make global but instead pass to every func that needs it
+    // // updategeom(); // TODO
+    // 
+    // /* init atoms */
+    // todo!();
 
 }
 
@@ -95,38 +95,38 @@ fn handle_event(event: Event, env: &Environment) {
     // TODO: add environment into here
     
     match event {
-        Event::ButtonPress(e) => button_press(e, env),
-        Event::ClientMessage(e) => client_message(e, env),
-        Event::ConfigureRequest(e) => configure_request(e, env),
-        Event::ConfigureNotify(e) => configure_notify(e, env),
-        Event::DestroyNotify(e) => destroy_notify(e, env),
-        Event::EnterNotify(e) => enter_notify(e, env),
-        Event::Expose(e) => expose(e, env), 
-        Event::FocusIn(e) => focus_in(e, env),
-        Event::KeyPress(e) => key_press(e, env),
-        Event::MappingNotify(e) => mapping_notify(e, env),
-        Event::MapRequest(e) => map_request(e, env),
-        Event::MotionNotify(e) => motion_notify(e, env),
-        Event::PropertyNotify(e) => property_notify(e, env),
-        Event::UnmapNotify(e) => unmap_notify(e, env),
+        // Event::ButtonPress(e) => button_press(e, env),
+        // Event::ClientMessage(e) => client_message(e, env),
+        // Event::ConfigureRequest(e) => configure_request(e, env),
+        // Event::ConfigureNotify(e) => configure_notify(e, env),
+        // Event::DestroyNotify(e) => destroy_notify(e, env),
+        // Event::EnterNotify(e) => enter_notify(e, env),
+        // Event::Expose(e) => expose(e, env), 
+        // Event::FocusIn(e) => focus_in(e, env),
+        // Event::KeyPress(e) => key_press(e, env),
+        // Event::MappingNotify(e) => mapping_notify(e, env),
+        // Event::MapRequest(e) => map_request(e, env),
+        // Event::MotionNotify(e) => motion_notify(e, env),
+        // Event::PropertyNotify(e) => property_notify(e, env),
+        // Event::UnmapNotify(e) => unmap_notify(e, env),
         _ => {}, // do nothing 
     };
 }
 
-fn run(conn: &RustConnection) {
+fn run(conn: &RustConnection, env: &Environment) {
     let running = true; 
-}
+
     loop {
         // TODO: this is just here to start, this needs to be double checked
         if !running { break; }
 
-        if let Some(event) = conn.wait_for_event() {
-            handle_event(event);
+        if let Ok(event) = conn.wait_for_event() {
+            handle_event(event, env);
         }
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let argv: Vec<String> = env::args().collect();
     let argc = argv.len();
     
@@ -145,23 +145,32 @@ fn main() {
     let (conn, screen_num) = x11rb::connect(None).unwrap();
 
     // TODO: do not believe this to be correct. 
-    let (sw, sh) = x11rb::protocol::randr::get_screen_info(conn, screen_num);
-    let env = Environment {
-        status_text: "",
-        screen_width: 0,
-    }
+    // let (sw, sh) = x11rb::protocol::randr::get_screen_info(conn, screen_num);
+    let screen = &conn.setup().roots[screen_num];
+    let win_id = conn.generate_id()?;
 
+    conn.create_window(
+        COPY_DEPTH_FROM_PARENT,
+        win_id, 
+        screen.root, 
+        0, 0, 
+        100, 100, 
+        0, WindowClass::INPUT_OUTPUT, 
+        0,
+        &CreateWindowAux::new().background_pixel(screen.white_pixel)
+        );
     
-    
-        .
-
-        .build();
-
+    // let env = Environment {
+    //     status_text: "",
+    //     screen_width: 0,
+    // };
     // todo: rest of main
-    check_other_wm(conn); // TODO: implement
-    setup(&conn);
-    scan();
-    run();
+    // check_other_wm(conn); // TODO: implement
+    // setup(&conn);
+    // scan();
+    // run();
     // cleanup();
+    
+    Ok(())
 
 }
