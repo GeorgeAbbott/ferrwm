6e4ae320918df0a78f43d49cc16b440c27751d2c
self <george@gabbott.dev>
2022-07-31 12:47:09 +0100

    Added some empty functions for client

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+use x11rb::protocol::xproto::Atom;
+
 use crate::tag::Tags;
 use crate::geom::Rect;
 
@@ -28,4 +30,35 @@ impl Client {
     pub fn unassign_tag(&mut self, tag: i32) {
         self.tags.unset(tag);
     }
+
+    #[allow(dead_code)]
+    /// Set the focus for a client.
+    pub fn set_focus(&mut self) {}
+
+    #[allow(dead_code)]
+    /// Send an event to the client, and returns a bool for 
+    /// whether the event exists. 
+    // TODO: check the above description is accurate.
+    pub fn send_event(&self, proto: Atom) -> bool { todo!(); }
+
+    // TODO: document.
+    pub fn set_state(&mut self, state: i64) { todo!(); }
+
+    /// Returns size hints, and whether any of these hints 
+    /// are different to the client's size.
+    pub fn apply_size_hints(&self, interact: bool) -> (Rect, bool) {
+        todo!();
+    }
+
+    /// Calls apply_size_hints, then resize.
+    pub fn hinted_resize(&mut self, rect: Rect, interact: bool) {
+
+    }
+
+    /// Resize the client to the given size.
+    pub fn resize(&mut self, rect: Rect) { todo!(); }
+
+
+
+
 }
